9ee4f24f8b3053cfff89241c9f8313d8418bc191
Pranav Hydrabade <pranav2329h@gmail.com>
2025-07-27 11:27:09 +0530

    demo

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { useState } from 'react'
+import Header from './Components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+    <Header/>
       <h1>Code step by step</h1>
     </>
   )
---
+++ first/src/Components/Header.jsx
@@ -0,0 +1,6 @@
+function Header(){
+    return <div>
+        <h4>This is the header</h4> 
+    </div>
+}
+export default Header;
