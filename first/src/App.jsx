61a65a2dfeff3704ac0e220fbd9d8276133011b5
Pranav Hydrabade <pranav2329h@gmail.com>
2025-07-27 17:59:10 +0530

    thodyas reaact work

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -1,13 +1,22 @@
 //import { useState } from 'react'
-import Header from './Components/Header'
+import Header,{Login,Settings, Userkey} from './Components/Header';
 
 function App() {
   return (
     <>
     <Header/>
+    <Login/>
+    <Settings/>
       <h1>Code step by step</h1>
+    <Fruit/>
+    <h1>{Userkey}</h1>
     </>
   )
 }
+export default App;
 
-export default App
+function Fruit(){
+  return(
+    <h1>Apple</h1>
+  )
+}
--- first/src/Components/Header.jsx
+++ first/src/Components/Header.jsx
@@ -3,4 +3,16 @@ function Header(){
         <h4>This is the header</h4> 
     </div>
 }
+export function Login(){
+    return(
+        <h1>Usser Login</h1>
+    )
+}
+export function Settings(){
+    return(
+        <h1>Settings</h1>
+    )
+}
 export default Header;
+
+export const Userkey="ewkjfgbrh";
