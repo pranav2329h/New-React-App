f3296b87f03be8befa9dc07fcbb75f55cc8ec5d6
Pranav Hydrabade <pranav2329h@gmail.com>
2025-10-14 18:22:50 +0530

    Refactor initial setup: update favicon, modify title, and simplify App component structure

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -1,22 +1,9 @@
-//import { useState } from 'react'
-import Header,{Login,Settings, Userkey} from './Components/Header';
-
 function App() {
   return (
-    <>
-    <Header/>
-    <Login/>
-    <Settings/>
-      <h1>Code step by step</h1>
-    <Fruit/>
-    <h1>{Userkey}</h1>
-    </>
-  )
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
 }
-export default App;
 
-function Fruit(){
-  return(
-    <h1>Apple</h1>
-  )
-}
+export default App;
--- first/src/main.jsx
+++ first/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+const myElements =(
+  <table>
+    <tr>
+      <td>1</td>
+      <td>2</td>
+      <td>3</td>
+      <td>4</td>
+    </tr>
+  </table>
+);
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  myElements
 )
